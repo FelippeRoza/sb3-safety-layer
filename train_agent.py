d38b5d1e72747d1435c81f8b8e398fe267c8f8b0
FelippeRoza <FelippeRoza@users.noreply.github.com>
2023-12-01 13:56:27 +0100

    improved experiment logging

--- train_agent.py
+++ train_agent.py
@@ -10,15 +10,19 @@ from costDynamicsModel import CDM
 
 def main(args):
 
+    data_dir = os.path.join('data', 'experiments', args.env_name, 'tensorboard')
     log_name = args.method if args.log_name == '' else args.log_name
+    if args.method == 'prob':
+        log_name = f'{log_name}_p{args.prob}'
+
     env = gym.make(args.env_name, render_mode = 'rgb_array')
 
     if args.method == 'unsafe':
-        rl_agent = SAC('MlpPolicy', env, verbose=1, tensorboard_log=os.path.join('data', 'experiments', args.env_name, 'tensorboard'))
+        rl_agent = SAC('MlpPolicy', env, verbose=1, tensorboard_log=data_dir)
     else:
         sl = CDM(env, buffer_size=args.sl_buffer_size)
-        sl.load(os.path.join('data', 'pretrained_cdm', args.env_name))
-        rl_agent = PPO(SafeActorCriticPolicy, env, verbose=1, tensorboard_log=os.path.join('data', 'experiments', 'tensorboard'),
+        sl.load(os.path.join('data', 'pretrained_cdm', args.env_name, f'linear_{True}'))
+        rl_agent = PPO(SafeActorCriticPolicy, env, verbose=1, tensorboard_log=data_dir,
                     policy_kwargs={'environment': env, 'safety_layer': sl, 'sl_mode': args.method})
 
     rl_agent.learn(total_timesteps=args.train_steps, log_interval=None, tb_log_name=log_name,
@@ -29,7 +33,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SB3SL: RL with a Safety Layer.')
 
-    parser.add_argument('--env_name', choices=['ContSafetyBallReach-v1', 'ContSafetyBallReach-v0'], 
+    parser.add_argument('--env_name', choices=['ContSafetyBallReach-v1', 'ContSafetyBallReach-v0', 
+                                               'MultiagentDescentralizedSafe-v0'], 
                         default='ContSafetyBallReach-v1')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--train_steps', type=float, default=1e6)
